7f824b2c44bc5bebd69df7b3f17bfc218cf46497
gmpcrm <gmpcrm@gmail.com>
2022-07-12 16:56:51 +0200

    math case

--- Lesson 2/hw4.py
+++ Lesson 2/hw4.py
@@ -3,11 +3,12 @@ while r < 1 or r > 4:
     print("Введите значение четверти от 1 до 4:")
     r = int(input())
 
-if r == 1:
-    print("x > 0 and y > 0")
-elif r == 2:
-    print("x < 0 and y > 0")
-elif r == 3:
-    print("x < 0 and y < 0")
-elif r == 4:
-    print("x > 0 and y < 0")
+match r:
+    case 1:
+        print("x > 0 and y > 0")
+    case 2:
+        print("x < 0 and y > 0")
+    case 3:
+        print("x < 0 and y < 0")
+    case 4:
+        print("x > 0 and y < 0")
