988747ba02cb41a10912acd591bdafd33390a32c
gmpcrm <gmpcrm@gmail.com>
2022-07-12 17:10:47 +0200

    -sqrt +round(2)

--- Lesson 2/hw5.py
+++ Lesson 2/hw5.py
@@ -1,5 +1,3 @@
-import math
-
 print("Введите координату x первой точки:")
 x1 = int(input())
 
@@ -12,5 +10,5 @@ x2 = int(input())
 print("Введите координату y второй точки:")
 y2 = int(input())
 
-result = math.sqrt((x2 - x1)**2 + (y2 - y1)**2).real
+result = round(((x2 - x1)**2 + (y2 - y1)**2)**0.5, 2)
 print(f"{result:.2f}")
