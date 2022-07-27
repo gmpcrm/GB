87a8a4ec19bd7650a5df87e4f8e6865472c1d388
gmpcrm <gmpcrm@gmail.com>
2022-07-27 14:24:59 +0200

    changes

--- Lesson 5/hw3.py
+++ Lesson 5/hw3.py
@@ -1,10 +1,11 @@
 import time
 
 k = 0
-while k < 1 or k > 20:
-    k = int(input("Задайте число от 1 до 20:"))
+while k < 1 or k > 32:
+    k = int(input("Задайте число от 1 до 32:"))
 
 nums = [(time.time_ns() >> value) & k for value in range(1, 64)]
-
 print(nums)
-print(set(nums))
+
+result = list(set(nums))
+print(result)
