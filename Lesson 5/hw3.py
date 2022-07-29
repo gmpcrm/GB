feefaf4d11e6255e97e7e1d19abc6f8f9ab17d1e
gmpcrm <gmpcrm@gmail.com>
2022-07-30 00:16:45 +0200

    Оптимизация

--- Lesson 5/hw3.py
+++ Lesson 5/hw3.py
@@ -4,15 +4,8 @@ k = 0
 while k < 1 or k > 32:
     k = int(input("Задайте число от 1 до 32:"))
 
-nums = [random.randrange(0, k) for value in range(0, 32)]
+nums = sorted([random.randrange(0, k) for value in range(0, 32)])
 print(nums)
 
-
-d = {}
-for num in nums:
-    d[num] = d.get(num, 0) + 1
-
-print(d)
-
-result = [val[0] for val in (filter(lambda x: x[1] == 1,d.items()))]
+result = [val for val in nums if nums.count(val) == 1]
 print(result)
