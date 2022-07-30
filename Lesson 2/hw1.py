2e293cbdc390982a7b55c627f6e86ad0684af2e5
gmpcrm <gmpcrm@gmail.com>
2022-07-30 20:39:09 +0200

    оптимизация

--- Lesson 2/hw1.py
+++ Lesson 2/hw1.py
@@ -1,9 +1,5 @@
 print("Введите день недели:")
 wd = int(input())
 
-if wd == 6 or wd == 7:
-    print("Выходной")
-elif wd > 0 and wd < 6:
-    print("Рабочий день")
-else:
-    print("Неправильный ввод")
+#оптимизация
+print("Выходной" if 5 < wd < 8 else "Рабочий день" if 0 < wd < 6 else "Неправильный ввод")
