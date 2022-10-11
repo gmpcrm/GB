16a4799b5b6c2cd21050c7862cce62af7c6c4a23
gmpcrm <gmpcrm@gmail.com>
2022-10-11 22:20:02 +0200

    поправлен вывод

--- Lesson 2/hw2.py
+++ Lesson 2/hw2.py
@@ -1,7 +1,7 @@
-print("x y z result")
+print("x\t\ty\t\tz\t\t¬(X ⋁ Y ⋁ Z)\t¬X ⋀ ¬Y ⋀ ¬Z\t¬(X ⋁ Y ⋁ Z) == ¬X ⋀ ¬Y ⋀ ¬Z")
 
 #оптимизация
-result = [" ".join(map(lambda x: str(int(not not x)), v)) 
-for v in [(x&4, x&2, x&1, not (x&1 or x&2 or x&4) == (not x&1 and not x&2 and not x&4)) 
+result = ["\t\t".join(map(lambda x: str(bool(not not x)), v)) 
+for v in [(x&4, x&2, x&1, not (x&1 or x&2 or x&4), (not x&1 and not x&2 and not x&4), not (x&1 or x&2 or x&4) == (not x&1 and not x&2 and not x&4)) 
 for x in range(0,8)]]
 print("\n".join(result))
